05ca158104a17f996ed016e207cd4d001b511fc9
jpseoane <juanpabloseoane@gmail.com>
2021-08-25 09:40:46 -0300

    dale

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const math = require('./math.js');
+/* const math = require('./math.js');
 
 console.log(math.divide(1,2));
 
@@ -6,5 +6,21 @@ console.log(math.add(1,2));
 
 console.log(math);
 
-console.log('Make it bold and red'.red.bold);
-''
+console.log('Make it bold and red'.red.bold); */
+
+
+const proxy_check = require('proxy-check');
+
+const proxy = {
+  host: '16.1.1.113',
+  port: 3128,
+  proxyAuth: 'y0adXjeO:pAzAHCr4'
+};
+// or
+// const proxy = 'y0adXjeO:pAzAHCr4@54.82.74.24:5557';
+
+proxy_check(proxy).then(r => {
+  console.log(r); // true
+}).catch(e => {
+  console.error(e); // ECONNRESET
+});
